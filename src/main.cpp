181fad99f7cda852c3f6be134728a4c99e420f5d
mauro <maurobra97@gmail.com>
2024-01-18 17:59:22 +0100

    Beautify main code

--- src/main.cpp
+++ src/main.cpp
@@ -117,10 +117,18 @@ void loop() {
 //         ((0, 1), 0), ((1, 1), 64), ((2, 1), 255)]
 // misura al centro del pixel quadrato e dimensione pixel parametrizzabile
 
-#include <WiFi.h>
-#include <WebServer.h>
-#include <SPIFFS.h>
-#include <ArduinoJson.h>
+// codice per leggere il tempo che intercorre tra un fronte e l'altro dei segnali del metal detector quando una certa distanza ? stata misurata con il mouse ottico
+// codice per ESP32 V2.2
+// il clock dell'ESP32 di default ? 240MHz
+// ATTENZIONE! Se il mouse non ? collegato e/o non ? propriamente alimentato, il codice si blocca nel setup aspettando che il mouse risponda
+// per l'ESP32, quando richiama una funzione da interrupt, questa deve essere precaricata in RAM tramite l'attributo IRAM_ATTR (invece che ICACHE_RAM_ATTR per ESP826)
+// coefficiente per mouse: 0.01151
+// esempio di testo per creare un'immagine con python:
+// dati = [((0, 0), 255), ((1, 0), 128), ((2, 0), 0),
+//        ((0, 1), 0), ((1, 1), 64), ((2, 1), 255)]
+// misura al centro del pixel quadrato e dimensione pixel parametrizzabile
+
+#include <Arduino.h>
 #include <PS2MouseHandler.h>
 
 #define MOUSE_DATA 18
@@ -151,28 +159,24 @@ unsigned long prevMillis = millis();
 int XVal = 0, YVal = 0;
 int Xprec = 0, Yprec = 0;
 float Xcm = 0, Ycm = 0;
-byte NCM = 5; // Numero di cm ogni quanto fare una misura
+byte NCM = 3; // Numero di cm ogni quanto fare una misura
 bool first = true; // Per capire se è la prima iterazione
 bool firstPython = true;
 bool OKXY = true; // Per capire se ho già misurato in un certo pixel
-const char *ssid = "Wall-Scanner";
-WebServer server(80);
-int resolution = 1; // TODO
 
-// Start del timer
+// Start timer
 void IRAM_ATTR startTimer() {
     noInterrupts();
-    if (!timerRunning) {  // se il timer non ? attivo, azzero il contatore
+    if (!timerRunning) { // Se il timer non è attivo, azzero il contatore
         timerCounter = ESP.getCycleCount();
         timerRunning = true;
     }
-
     interrupts();
 }
 
-// Stop del timer
+// Stop timer 
 void IRAM_ATTR stopTimer() {
-    if (timerRunning) { // Se in corso, fermo
+    if (timerRunning) {
         elapsedTime = ESP.getCycleCount() - timerCounter;
         noInterrupts();
         timerRunning = false;
@@ -198,7 +202,7 @@ void LedPWM() {
     }
 }
 
-/*
+/* Stampa python
 void stampaPython(int X, int Y, float mag) {
     if (firstPython) {
         firstPython = false;
@@ -222,7 +226,7 @@ void stampaPython(int X, int Y, float mag) {
 }
 */
 
-// Stampo sulla seriale
+// Stampo valori
 void stampaNormale(int X, int Y, float mag) {
     Serial.print(X);
     Serial.print(",");
@@ -231,7 +235,7 @@ void stampaNormale(int X, int Y, float mag) {
     Serial.println(mag, 1);
 }
 
-// Controller LED per capire in che direzione muoversi
+// Gestisco LED per direzione scansione
 void LEDUpDown(float Ycm, int Yprec) {
     // Accendo il LED centrale
     if (Ycm - int((Ycm / NCM) * NCM > float(NCM) / 3) && (Ycm - int(Ycm / NCM) * NCM < float(NCM) * 2 / 3)) {
@@ -255,92 +259,10 @@ void LEDUpDown(float Ycm, int Yprec) {
     }
 }
 
-// Spezzo il file e mando il contenuto al client
-void splitAndSend(const char* path, const char* type) {
-    File file = SPIFFS.open(path, "r");
-    if (!file) { // Controllo se il file è stato caricato correttamente
-        server.send(500, "text/plain", "Errore interno del server");
-        return;
-    }
-
-    // Spezzo il file e mando
-    server.setContentLength(file.size());
-    server.send(200, type, "");
-    const size_t bufferSize = 1024;
-    char buffer[bufferSize];
-    while (file.available()) {
-        size_t len = file.readBytes(buffer, bufferSize);
-        server.sendContent_P(buffer, len);
-    }
-    file.close(); // Chiudo il file
-}
-
-// faccio partire il server web
-void setServerWeb() {
-    if (!SPIFFS.begin(true)) { // Inizializzo SPIFFS
-        Serial.println("Errore nell'inizializzazione di SPIFFS");
-        return;
-    }
-
-    // Configuro l'ESP come Access Point
-    WiFi.softAP(ssid);
-
-    // Routes file
-    server.on("/", HTTP_GET, []() {
-        splitAndSend("/index.html", "text/html");
-    });
-    server.on("/css/bootstrap.min.css", HTTP_GET, []() {
-        splitAndSend("/css/bootstrap.min.css", "text/css");
-    });
-    server.on("/css/style.css", HTTP_GET, []() {
-        splitAndSend("/css/style.css", "text/css");
-    });
-    server.on("/js/bootstrap.bundle.min.js", HTTP_GET, []() {
-        splitAndSend("/js/bootstrap.bundle.min.js", "text/javascript");
-    });
-    server.on("/js/jquery-3.7.1.min.js", HTTP_GET, []() {
-        splitAndSend("/js/jquery-3.7.1.min.js", "text/javascript");
-    });
-    server.on("/js/rainbowVis.min.js", HTTP_GET, []() {
-        splitAndSend("/js/rainbowVis.min.js", "text/javascript");
-    });
-    server.on("/js/script.js", HTTP_GET, []() {
-        splitAndSend("/js/script.js", "text/javascript");
-    });
-    server.on("/img/green-checkmark.svg", HTTP_GET, []() {
-        splitAndSend("/img/green-checkmark.svg", "image/svg+xml");
-    });
-
-    // Routes servizi
-    server.on("/settings", HTTP_GET, []() {
-        JsonDocument doc;
-        doc["resolution"] = resolution;
-        String json;
-        serializeJson(doc, json);
-        server.send(200, "application/json", json);
-    });
-    server.on("/settings", HTTP_POST, []() {
-        String body = server.arg("plain");
-        JsonDocument doc;
-        DeserializationError error = deserializeJson(doc, body);
-        if (error) { // Controllo se il formato JSON è corretto
-            server.send(400, "text/plain", "Errore nel formato JSON");
-            return;
-        }
-
-        resolution = doc["resolution"]; // Sovrascrivo risoluzione
-        doc["resolution"] = resolution; // Mando la variabile aggiornata al front-end
-        String json;
-        serializeJson(doc, json);
-        server.send(200, "application/json", json);
-    });
-
-    // Avviare il server
-    server.begin();
-}
-
-// Setup dei PIN
-void setupPin() {
+// Setup
+void setup() {
+    noInterrupts();
+    Serial.begin(115200);
     pinMode(BUTTON, INPUT_PULLUP);
     pinMode(TXPIN, INPUT);
     pinMode(RXPIN, INPUT);
@@ -353,26 +275,13 @@ void setupPin() {
     ledcAttachPin(REDLED, REDCH);
     ledcSetup(YELLCH, 1000, 8);
     ledcAttachPin(GREENLED, YELLCH);
-}
-
-// Setup
-void setup() {
-    Serial.begin(115200);
-    setServerWeb(); // Faccio partire il server web
-    noInterrupts();
-    setupPin(); // Inizializzo i pin
-
-    // Inizializzo il mouse
-    if (mouse.initialise() != 0) { // Errore nell'inizializzazione del mouse
+    if (mouse.initialise() != 0) // Mouse error
         Serial.println("mouse error");
-    }
-
-    interrupts(); // Ripristino interruzioni
+    interrupts();
 }
 
 // Loop
 void loop() {
-    server.handleClient();
     switch (stato) {
         case 0:
             if (!digitalRead(BUTTON)) {
@@ -383,7 +292,7 @@ void loop() {
                 attachInterrupt(RXPIN, stopTimer, FALLING);
             }
             break;
-        
+
         case 1: // Taro la bobina
             if (first) {
                 first = false;
@@ -392,7 +301,7 @@ void loop() {
 
             if (!printed) {
                 if (i < 60) {
-                    delta = delta + elapsedTime / 240;
+                    delta = delta + elapsedTime / 240; // in micros
                     elapsedTime = 0;
                     i++;
                     delayMicroseconds(200);
@@ -411,14 +320,13 @@ void loop() {
                 stato = 2;
                 Serial.println("----------");
             }
-
             interrupts();
             break;
 
         case 2: // Taro il delay
             if (!printed) {
                 if (i < 5000) {
-                    Fi0 = Fi0 + float(elapsedTime) / 240;
+                    Fi0 = Fi0 + float(elapsedTime) / 240; // In micros
                     elapsedTime = 0;
                     i++;
                     delayMicroseconds(200);
@@ -435,7 +343,6 @@ void loop() {
                     stato = 3;
                 }
             }
-
             interrupts();
             break;
 
@@ -453,12 +360,7 @@ void loop() {
                 YVal += mouse.y_movement();
                 Xcm = XVal * 0.01151;
                 Ycm = YVal * 0.01151;
-
-                // Serial.print("Xcm-int((Xcm/NCM)*NCM: ");
-                // Serial.println(Xcm-int((Xcm/NCM)*NCM));
-
                 LEDUpDown(Ycm, Yprec);
-
                 if (Xprec != int(Xcm) / NCM | Yprec != int(Ycm) / NCM) {
                     Xprec = int(Xcm) / NCM;
                     Yprec = int(Ycm) / NCM;
@@ -476,7 +378,7 @@ void loop() {
         case 4: // Misuro magnetismo e torno a misurare
             if (!printed) {
                 if (i < 500) {
-                    delta = delta + elapsedTime / 240;
+                    delta = delta + elapsedTime / 240; // In micros
                     elapsedTime = 0;
                     i++;
                     delayMicroseconds(200);
@@ -484,7 +386,6 @@ void loop() {
                     i = 0;
                     delta = delta / 501;
                     stampaNormale(int(Xcm / NCM), int(Ycm / NCM), delta);
-                    // stampaPython(int(Xcm/NCM), int(Ycm/NCM), delta);
                     printed = true;
                     LedPWM();
                     stato = 5;
@@ -495,6 +396,9 @@ void loop() {
 
         case 5:
             stato = 3;
+            break;
+
+        case 6:
             break;
     }
 }
